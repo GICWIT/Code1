bbf2681d41cfc81bc2e4d5bd30cc8264b27ba6f9
Levi Manning <ldmanning@gicw.org>
2016-02-02 11:29:52 -0800

    Add entire Solution to repo

---
+++ Hello World/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    /// <summary>
+    /// this namespace is neat
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// this class does things
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+            //Tell the world hello
+            Console.WriteLine("Hello World!");
+
+            /*Wait for the 
+            user*/
+            Console.ReadLine();
+        }
+    }
+}
+
